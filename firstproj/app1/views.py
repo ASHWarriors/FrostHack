3effa2b395449e9593d5122b486ae7b5ffb3c7c5
Sristi <73350682+S-JZ@users.noreply.github.com>
2021-05-09 13:21:22 +0530

    Delete firstproj directory

--- firstproj/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def first(request):
-    return render(request,'index.html')
-
-def second(request):
-    return render(request,'dsc_sesh1_b.html')
